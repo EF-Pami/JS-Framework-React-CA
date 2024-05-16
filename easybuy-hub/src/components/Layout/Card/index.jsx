3a67931e67b09d2e49280e08e6a4ebadb021e8bf
EF-Pami <pamijoelpami@gmail.com>
2024-05-16 14:46:10 +0200

    added contents for cart icon

---
+++ easybuy-hub/src/components/Layout/Card/index.jsx
@@ -0,0 +1,15 @@
+import { ShoppingCart } from "@mui/icons-material"
+import { useCart } from "../../../zustandHook/cartstore"
+
+export default function cartIcon() {
+    const {cart} = useCart();
+
+return (
+    <div>
+        <ShoppingCart fontSize="large" style={{color: 'black'}}/>
+        <span>
+            <span>{cart.reduce((totalQty, cart) => totalQty + cart.totalItems, 0)}</span>
+        </span>
+    </div>
+)
+}
