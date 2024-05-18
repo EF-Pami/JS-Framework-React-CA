e0913c40a6d78d8495d65a34ac7857f68023ef15
EF-Pami <pamijoelpami@gmail.com>
2024-05-18 14:10:11 +0200

    added NotFound.styles

---
+++ easybuy-hub/src/components/Styles/NotFound.styles.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const styledNotFound = styled.div`
+display:flex;
+flex-direction: column;
+
+div, h1, p {
+    display:flex;
+    justify-content:center;
+}
+`
