a58a454e8191d67839a7c4a7c42890010cd15edc
EF-Pami <pamijoelpami@gmail.com>
2024-05-18 14:14:19 +0200

    added Error.styles

---
+++ easybuy-hub/src/components/Styles/Error.styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const styledError = styled.div`
+display: flex;
+justify-content:center;
+align-items:center;
+margin-bottom: 32px;
+font-size: 22px;
+color:red;
+`
