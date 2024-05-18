6eceb49b606ff9dbf90c99ca756dc1c71b8e24c4
EF-Pami <pamijoelpami@gmail.com>
2024-05-18 15:18:49 +0200

    added ProductReviews.styles

---
+++ easybuy-hub/src/components/Styles/ProductReviews.styles.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const styledProductReviews = styled.div`
+margin: 0 2rem;
+
+@media (max-width: 600px) {
+    display:flex;
+    flex-direction:column;
+
+    h3 {
+        display:flex;
+        justify-content:center;
+    }
+}
+`
+
+export const styledWrapper = styled.div`
+display:flex;
+justify-content: center;
+@media (max-width: 600px) {
+    display:flex;
+    flex-direction: column;
+    margin-bottom: 40px;
+}
+`
+export const StyledContainer = styled.div`
+padding: 10px;
+margin: 10px;
+border-radius: 10px;
+border: 1px solid ${({theme}) => theme.colors.black};
+`
+
+export const StyledDesc = styled.div`
+margin-bottom:20px;
+`
+
+export const StyledUsername = styled.div`
+margin: 10px;
+`
+
+export const StyledRating = styled.div`
+display: flex;
+align-items: center;
+`
