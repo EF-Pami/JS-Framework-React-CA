c50faa487436541be6767c091f2ad88ce46669cf
EF-Pami <pamijoelpami@gmail.com>
2024-05-19 13:12:35 +0200

    added CheckoutSuccess/index.jsx

---
+++ easybuy-hub/src/components/CheckoutSuccess/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyledWrapper } from "../Styles/Success.styles";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { ArrowBackIosNew } from "@mui/icons-material";
+
+export default function CheckoutMessage() {
+    return (
+        <StyledWrapper>
+            <CheckCircle fontSize="large" style={{color: '#00C013'}}/>
+            <H1>Thanks for shopping with EasyBuy-Hub</H1>
+            <p>Your Order number: E000B012H</p>
+            <P>Check your email for more information on order confirmation and delivery</P>
+            <Link to="/" style={{ textDecoration: 'none'}}>
+                <button><ArrowBackIosNew />Continue Shopping</button>
+            </Link>
+        </StyledWrapper>
+    )
+}
