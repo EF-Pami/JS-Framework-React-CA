b2c92fd06e393e697eedc3e2e6431f783f36b0cd
EF-Pami <pamijoelpami@gmail.com>
2024-05-19 15:52:48 +0200

    added Loader/index.jsx

---
+++ easybuy-hub/src/components/Loader/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledLoader } from "../Styles/Loader.styles";
+
+export default function Loader() {
+    return (
+        <StyledLoader></StyledLoader>
+    )
+}
