7934a9c5019c9ad3619ff4f6a5e47cacf9eed7b5
EF-Pami <pamijoelpami@gmail.com>
2024-05-19 11:09:55 +0200

    added Search/index.jsx

---
+++ easybuy-hub/src/components/Search/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Search } from "@mui/icons-material";
+import * as S from "../Styles/Search.styles";
+
+/**
+ * this code action will build the search bar and permit users to search for items
+ */
+
+export default function SearchBar ({searchInput, onSearchInputChange, filteredProducts}) {
+    
+    function onChange (event) {
+        onSearchInputChange(event.currentTarget.value)
+    }
+
+    return (
+        <S.StyledSearch>
+            <S.StyledInputWrapper>
+                <Search
+                    fontSize="large"
+                    sx={{color: "black"}}
+                />
+                <S.StyledInput
+                    onChange={onChange}
+                    value={searchInput}
+                    placeholder="Search item"
+                />
+            </S.StyledInputWrapper>
+            <S.StyledSearchResults>
+                {filteredProducts.map((item) => (
+                    <S.StyledSearchedItem to={`/products/${item.id}`} key={item.id}>
+                        {item.title}
+                    </S.StyledSearchedItem>
+                ))}
+            </S.StyledSearchResults>
+        </S.StyledSearch>
+    )
+}
