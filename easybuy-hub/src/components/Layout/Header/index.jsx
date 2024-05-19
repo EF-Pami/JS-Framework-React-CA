949de2429df5f9b036359596c1f73e598a56fee7
EF-Pami <pamijoelpami@gmail.com>
2024-05-19 11:52:43 +0200

    added Header/index.jsx

---
+++ easybuy-hub/src/components/Layout/Header/index.jsx
@@ -0,0 +1,11 @@
+import Nav from "../Nav";
+import { styledHeader } from "../../Styles/Header.styles";
+
+export default function Header() {
+    return (
+        <styledHeader>
+            <Nav />
+        </styledHeader>
+    )
+}
+
